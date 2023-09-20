9d45cad15e30166b460e7bfa34c919764b61849a
CaroBaiadera <carobaiadera@gmail.com>
2023-09-20 19:45:51 +0200

    fisrt commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+//Este es el comentario para una sola linea
+/*Este es un comentario en bloque
+si quiero lo sigo aca bajo*/ 
+console.log("Hola mundos desde un archivo JS")
+
+//Declaracion de varialbes
+
+let nombre = "Arturo ";
+console.log(nombre)
+
+let apellido = prompt("Indique su apellido")
+document.write("Mi nombre completo es ", nombre, apellido)
+
+var numero = 10 + 5;
+let booleano = false
+const pi = 3.14159
+
+let prueba = Number('Arturo')
+console.log(prueba)
+
+pi = 10;
+console.log(pi)
+
+
+
+let x;
+
